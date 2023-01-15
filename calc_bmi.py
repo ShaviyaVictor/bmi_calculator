130080dee0fd29bbfa85a3093da3a9272a94bdb9
shaviyavictor <shaviyavictor@gmail.com>
2023-01-15 16:16:54 -0500

    Refine the program

--- calc_bmi.py
+++ calc_bmi.py
@@ -10,39 +10,41 @@ print(
 )
 name = input()
 
-print("\nHey, " + name + "." "Let's begin by finding out what your body weight is?\n")
-weight = input()
+print(
+  "\nHey, " + name + "." " Let's begin by finding out what your body weight(in kg) is?\n"
+)
+weight = int(input())
 
 print(
   """
-  \nCool!
+  Cool!
   .
-  How about your height?\n
+  How about your height(in cm)?\n
   """
 )
 height = int(input())
 
-bmi = weight/height
+bmi = (weight / height) * 100
 
 if bmi < 18.5:
   print(
     """
-    \nHuh, we need to add some more weight.
+    Huh, we need to add some more weight.
     You fall within the underweight range..\n
     """
   )
-elif bmi > 18.5 & bmi < 24.9:
+elif bmi > 18.5 and bmi < 24.9:
   print(
-    "\nHey " + name + ",\n" + bmi + " is within the Healthy Weight range.\n" + "Awesome!\n"
+    f"Hey " + name + ",\n" + str(bmi) + " is within the Healthy Weight range.\n" + "Awesome!\n"
   )
-elif bmi > 25.0 & bmi < 29.9:
+elif bmi > 25.0 and bmi < 29.9:
   print(
     """
-    \nHuh, we need to cut it up a little.
+    Huh, we need to cut it up a little.
     You fall within the overweight range..\n
     """
   )
 else:
   print(
-    "\nWhoa! " + bmi + " is within obese range!!!"
+    f"Whoa! " + str(bmi) + " is within obese range!!!"
   )
